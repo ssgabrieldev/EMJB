0ff882d72479a74450249682d6abd64003c605d1
Paulo Gabriel <ssgabrieldev@gmail.com>
2024-09-07 09:33:04 -0300

    setup new firebase project

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { browserLocalPersistence, getAuth, setPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA-FMLHn-ko5kYNyou4eEfwpYZZyd2gTSc",
-  authDomain: "emjb-25396.firebaseapp.com",
-  projectId: "emjb-25396",
-  storageBucket: "emjb-25396.appspot.com",
-  messagingSenderId: "985820330830",
-  appId: "1:985820330830:web:ea4db50895c0e6bbd03c1f",
-  measurementId: "G-SZC5X3PH8X"
-};
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
